7e798cedf471b46067981e0abe51cc8478687e19
uviajmuons <uijuncoding@gmail.com>
2025-02-14 16:24:49 +0900

    update
    home, footer

--- views/home.php
+++ views/home.php
@@ -1,17 +1,28 @@
 <?php
-  $board = DB::fetchAll("select * from board order by time");
+  $user_id;
+  $board = DB::fetchAll("select * from board order by time desc");
 ?>
+
 <main class="main-board con">
   <section class="board-list">
     <?php foreach ($board as $b) {  ?>
-      <a href="/board">
+      <?php $user_id = $b->user_id; ?>
+      <?php $profileImg = DB::fetch("select img from user where id = '$user_id'"); ?>
+      <a href="/board" class="board-item-container">
         <article class="w1 board-item jb ac">
-          <div class="board-user-info fx">
-            <img src="" alt="profile" />
+          <div class="board-user-info fc ac">
+            <?php if ($profileImg->img) { ?>
+              <img src="<?= $b->img ?>" alt="profile" class="board-user-profile" />
+            <?php } else { ?>
+              <div class="board-user-profile fb">NP</div>
+            <?php } ?>
             <p class="user-id"><?= $b->user_id ?></p>
           </div>
           <h2 class="board-title"><?= $b->title ?></h2>
-          <p class="time"><?= $b->time ?></p>
+          <div class="fx ac">
+            <p class="time"><?= $b->time ?></p>
+            <p class="likes"><b>❤️</b>&nbsp;<?= $b->likes ?></p>
+          </div>
         </article>
       </a>
     <?php } ?>
@@ -20,3 +31,8 @@
     <a href="/post" class="btn fb hov abs post">✏️Post</a>
   <?php } ?>
 </main>
+
+<script>
+  function generateRandomHexColor() { return `#${((r, g, b) => (r = Math.floor(Math.random() * 156) + 100, g = Math.floor(Math.random() * 156) + 100, b = Math.floor(Math.random() * 156) + 100, `${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`))(0, 0, 0)}`; }
+  $$('div.board-user-profile').forEach(({ style }) => style.backgroundColor = generateRandomHexColor());
+</script>
--- views/template/footer.php
+++ views/template/footer.php
@@ -1,6 +1,7 @@
   <footer class="w1 gradient">
-    <div class="con">
-      
+    <div class="con fx ac h1">
+      <a href="/" class="hov sub-logo round"><h1>FakeBook</h1></a>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores repudiandae, libero soluta iure illum quia perspiciatis inventore assumenda id reprehenderit aut ducimus atque nesciunt cumque aperiam voluptas aliquam autem, voluptatibus a velit incidunt eius dolorum adipisci omnis? Vel facere et voluptate animi voluptatum culpa eaque nam. Facilis molestiae eum unde excepturi aperiam? Corporis atque quod molestias deleniti eaque ipsam at.</p>
     </div>
   </footer>
 </body>
